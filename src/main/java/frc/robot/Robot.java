80bb85de8b3866b8feaad5b14c8b087659a8c705
T3 FRC Team 7312 <79057169+t3team-laptop@users.noreply.github.com>
2023-02-06 15:56:55 -0600

    maxSpeed change

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -57,6 +57,8 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {}
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+
+  /* 
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
@@ -66,6 +68,7 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.schedule();
     }
   }
+  */
 
   /** This function is called periodically during autonomous. */
   @Override
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -147,8 +147,11 @@ public class RobotContainer {
      *
      * @return the command to run in autonomous
      */
+
+    /* 
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
         return new exampleAuto(s_Swerve);
     }
+    */
 }
