e90ef1e2f42205e7eb56aa7168768c879f661754
T3 FRC Team 7312 <79057169+t3team-laptop@users.noreply.github.com>
2023-03-01 16:14:53 -0600

    still crying

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
@@ -185,6 +186,13 @@ public class RobotContainer {
         DX.onTrue(xLock);
     }
 
+    private void configureSmartDashboard() {
+        autoChooser.setDefaultOption("Move forward", moveForward);
+        autoChooser.addOption("S curve", sCurve);
+        autoChooser.addOption("SUSSY - CADEN", sussy);
+    
+        SmartDashboard.putData(autoChooser);
+      }
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
      *
